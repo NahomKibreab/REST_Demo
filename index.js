74428d58ff4b3336469d0539704aaf3b651dd0c2
NahomKibreab <nknahom@gmail.com>
2021-01-22 18:52:50 -0700

    Express Method Override

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -54,6 +58,12 @@ app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
   const comment = comments.find((comment) => comment.id === id);
   res.render('comments/show', { comment });
+});
+
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((comment) => comment.id === id);
+  res.render('comments/edit', { comment });
 });
 
 app.patch('/comments/:id', (req, res) => {
