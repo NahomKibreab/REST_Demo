139eb87e06c34d1061d31018e1d6198284c6a833
NahomKibreab <nknahom@gmail.com>
2021-01-22 17:24:55 -0700

    RESTful Comments Show

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 const comments = [
-  { username: 'Todd', comment: 'lol that is so funny' },
   {
+    id: 1,
+    username: 'Todd',
+    comment: 'lol that is so funny',
+  },
+  {
+    id: 2,
     username: 'Skyler',
     comment: 'I like to go birdwatching with my dog',
   },
   {
+    id: 3,
     username: 'Sk8erBoi',
     comment: 'Plz delete your accound, todd',
   },
   {
+    id: 4,
     username: 'onlysayswoof',
     comment: 'woof woof woof',
   },
@@ -40,6 +47,12 @@ app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment });
   res.redirect('/comments');
+});
+
+app.get('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((comment) => comment.id === parseInt(id));
+  res.render('comments/show', { comment });
 });
 
 app.get('/tacos', (req, res) => {
